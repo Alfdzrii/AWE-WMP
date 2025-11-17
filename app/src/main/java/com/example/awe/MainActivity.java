a27e6019aa92147266c15e7eb192b4d9f1d1fbe9
Alfdzrii <Alfadzri.abhipraya@gmail.com>
2025-11-17 09:36:57 +0700

    adding button function

--- app/src/main/java/com/example/awe/MainActivity.java
+++ app/src/main/java/com/example/awe/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.awe;
-
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
 public class MainActivity extends AppCompatActivity {
+    Button BtnRegist, BtnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,4 +33,6 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+
 }
